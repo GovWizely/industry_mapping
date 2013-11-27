7842ee6f261aa96a67f2357e3ad8687ffbf9ed2a
Loren Siebert <loren@siebert.org>
2013-11-27 13:22:46 -0800

    Tweaked API route to default to JSON format

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 IndustryMapping::Application.routes.draw do
-  resources :emenus
-
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root :to => "home#index"
-  get "industry_sector(.json)" => "sectors#lookup", format: false
+  get "industry_sector(.json)" => "sectors#lookup", defaults: { format: :json }
 end

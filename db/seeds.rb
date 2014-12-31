1934db7dbe2746eef83f6995f7ac7a16b748afda
Calum Halcrow <calumhalcrow@gmail.com>
2014-12-31 13:20:51 +0800

    Assume all topics belong to MarketResearch.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,11 @@
 require 'csv'
 
-default_source = Source.find_or_create_by(name: 'default')
-
 CSV.foreach("#{Rails.root}/lib/data/industry_taxonomy.csv", :headers => :first_row) do |row|
-  industry_str, industry_sector_str, topic_industry_code_str, topic_industry_str = row[0], row[1], row[2], row[3]
-  industry = Industry.find_or_create_by!(name: industry_str.strip)
-  sector = industry.sectors.find_or_create_by!(name: industry_sector_str.strip)
-  sector.topics.find_or_create_by(name: topic_industry_str.strip) do |t|
-    t.source = default_source
-  end
+  industry_name, sector_name, topic_name, source_name = row[0], row[1], row[2], row[3]
+
+  industry = Industry.find_or_create_by!(name: industry_name)
+  source = Source.find_or_create_by!(name: source_name)
+
+  sector = industry.sectors.find_or_create_by!(name: sector_name)
+  sector.topics.find_or_create_by!(name: topic_name, source_id: source.id)
 end

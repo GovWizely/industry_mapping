20d999ccb0f27f161cc61a43b44056c8e7348e12
Calum Halcrow <calumhalcrow@gmail.com>
2015-02-23 14:14:29 +0800

    find_or_create_by over associations.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@ CSV.foreach("#{Rails.root}/lib/data/industry_taxonomy.csv", :headers => :first_r
   source = Source.find_or_create_by!(name: source_name)
 
   sector = industry.sectors.find_or_create_by!(name: sector_name)
-  sector.topics.find_or_create_by!(name: topic_name, source_id: source.id)
+  sector.topics.find_or_create_by!(name: topic_name, source: source)
 end

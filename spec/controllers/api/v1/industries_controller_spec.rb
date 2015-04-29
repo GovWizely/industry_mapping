4cbd6d862a6685f3be5c576ab37e920631503b80
baraabourghli <baraa.bourghli@gmail.com>
2015-04-29 23:25:47 +0800

    cleanup specs for record failed lookups

--- spec/controllers/api/v1/industries_controller_spec.rb
+++ spec/controllers/api/v1/industries_controller_spec.rb
@@ -3,10 +3,6 @@ require 'spec_helper'
 describe Api::V1::IndustriesController, type: :controller do
   render_views
 
-
-    #sector
-    #topic
-    #source
   let(:query) { {} }
   let(:get_index) { get :index, query }
   let(:json_response) { JSON.parse(response.body) }
@@ -72,22 +68,6 @@ describe Api::V1::IndustriesController, type: :controller do
     end
   end
 
-  context 'sector only' do
-  end
-
-  context 'topic and source' do
-# Check that no Failed Lookup was created.
-  end
-
-  context 'topic and sector' do
-  end
-
-  context 'source and sector' do
-  end
-
-  context 'all params' do
-  end
-
   context 'when requesting to log failed lookups' do
     context 'with source and topic' do
       context "when source doesn't exist" do
@@ -123,90 +103,4 @@ describe Api::V1::IndustriesController, type: :controller do
       end
     end
   end
-
-
-  #context 'when endpoint is unknown' do
-
-    #it { is_expected.to respond_with(:success) }
-
-    #it 'has no industries in response' do
-      #json_response = JSON.parse(response.body)
-      #expect(json_response.count).to eq(0)
-    #end
-  #end
-
-  #describe 'industries endpoint' do
-    #context 'when only topic param is present' do
-      #context 'and is right name' do
-        #before { get :index, topic: topic.name }
-
-        #it { is_expected.to respond_with(:success) }
-
-        #it 'has correct JSON in response' do
-          #json_response = JSON.parse(response.body)
-          #expect(json_response[0]['id']).to eq( topic.sector.industry.id)
-          #expect(json_response[0]['name']).to eq( topic.sector.industry.name)
-        #end
-      #end
-
-      #context 'and wrong name' do
-        #before { get :index, topic: 'unknown!' }
-
-        #it { is_expected.to respond_with(:success) }
-
-        #it 'returns empty JSON array in response' do
-          #json_response = JSON.parse(response.body)
-          #expect( json_response.length ).to eq 0
-        #end
-      #end
-      #it "does not change Topic number" do
-        #expect { get :index, topic: topic.name }.to_not change{ Topic.count }
-        #expect { get :index, topic: 'unknown!' }.to_not change{ Topic.count }
-      #end
-    #end
-
-    #context 'when topic param and source are present' do
-      #context 'and are right named' do
-        #before { get :index, topic: topic.name, source: topic.source.name }
-
-        #it { is_expected.to respond_with(:success) }
-
-        #it 'has correct JSON in response' do
-          #json_response = JSON.parse(response.body)
-          #expect( json_response[0]['id']).to eq( topic.sector.industry.id)
-          #expect( json_response[0]['name']).to eq( topic.sector.industry.name)
-        #end
-      #end
-
-      #context 'and the topic is unknown' do
-        #let(:param) { {topic: 'unknown!', source: topic.source.name} }
-
-        #it 'responds with success' do
-          #get :index, param
-          #expect(response).to have_http_status(:success)
-        #end
-
-        #it 'returns empty JSON array in response' do
-          #get :index, param
-          #json_response = JSON.parse(response.body)
-          #expect( json_response.length ).to eq 0
-        #end
-      #end
-
-      #context 'and the source is unknown but topic is correct' do
-        #let(:param) { {topic: topic.name, source: 'unknown!'} }
-
-        #it 'response with success' do
-          #get :index, param
-          #expect(response).to have_http_status(:success)
-        #end
-
-        #it 'returns empty JSON array in response' do
-          #get :index, param
-          #json_response = JSON.parse(response.body)
-          #expect(json_response.length).to eq 0
-        #end
-      #end
-    #end
-  #end
 end

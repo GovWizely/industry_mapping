92a00b4a0ec10000fe12f6c1aa74fb893402b801
Calum Halcrow <calum@rrsoft.co>
2015-05-11 18:10:15 +0800

    root to admin dashboard.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 IndustryMapping::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root :to => "home#index"
+  root :to => "admin/dashboard#index"
 
   namespace :api, defaults: {format: 'json'} do
     scope module: :v1 do
